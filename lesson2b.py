8efa5ddc22d691eb702ee8923770e0b5d97295ef
vedshah0207-bit <vedshah0207@gmail.com>
2026-09-24 18:35:43 +0400

    I learnt lots of print statements!

---
+++ lesson2b.py
@@ -0,0 +1,8 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+codingal = "Codingal"
+print(codingal)
+name = input("Please enter your name: ")
+print("\nHello", name, "\nWelcome to Codingal!")
---
+++ lesson2c.py
@@ -0,0 +1,3 @@
+import keyword
+#this will print all the python keywords
+print("Python keywords are:\n", keyword.kwlist)
